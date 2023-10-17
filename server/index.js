95644d1ab5559c3e3bba6b83e239eb7a4ce4146d
Katie K <katiekucsera@gmail.com>
2023-10-17 17:10:48 -0400

    Remove changes

--- server/index.js
+++ server/index.js
@@ -35,7 +35,3 @@ app.use("/tweets", tweetsRoutes);
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
-
-app.post("/tweets", (req, res) => {
-  // console.log(req.body, data);
-});
